8896b65ea619dabfaaa464bbe52cecf89f0fb2f2
KhailoAndrey <hap512@gmail.com>
2023-02-11 16:18:57 +0200

    coding

--- src/functions.js
+++ src/functions.js
@@ -1,1 +1,33 @@
+export class Schedules {
+//   #URL: "https://school-schedule-6e59d-default-rtdb.europe-west1.firebasedatabase.app/schedules.json";
+  //   constructor(URL) {
+  //     this.URL = date;
+  //   }
+  static create(schedule) {
+    return fetch(
+      "https://school-schedule-6e59d-default-rtdb.europe-west1.firebasedatabase.app/schedules.json",
+      {
+        method: "POST",
+        body: JSON.stringify(schedule),
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }
+    )
+      .then((response) => response.json())
+      .then((response) => console.log(response));
+  }
+}
 
+// class options {
+
+// }
+// export function fetchData(date, method) {
+//     fetch(schedules.URL, options)
+//       .then((response) => {
+//         if (!response.ok) {
+//           throw new Error(response.status);
+//         }
+//         return response.json();
+//       });
+// }
--- src/index.js
+++ src/index.js
@@ -1,27 +1,43 @@
-import "./styles.css";
 import flatpickr from "flatpickr";
 import "flatpickr/dist/flatpickr.min.css";
-// import {submitHandler} from "./functions"
-
-let date = "";
+import { Schedules } from "./functions";
+import "./styles.css";
 
 const submitBtn = document.getElementById("submit-form");
 const dateForm = document.getElementById("datetime-picker");
+const cardSchedule = document.getElementById("schedule-list");
+const inputLesson = document.getElementById("input-lesson");
+
+let userDate = new Date();
 
 const options = {
   defaultDate: new Date(),
   dateFormat: "d-m-Y",
   onClose(dateStr) {
-    date = dateStr;
+    userDate = dateStr;
   },
 };
 flatpickr(dateForm, options);
+
 submitBtn.addEventListener("click", submitHandler);
 
 function submitHandler(e) {
   e.preventDefault();
-  if (date === "") {
-    date = new Date();
+
+  const schedule = {
+    date: userDate,
+    lesson: inputLesson.value,
+  };
+
+  Schedules.create(schedule);
+}
+
+function createCardSchedule(countLesson, lesson) {
+  cardSchedule.innerHTML = "";
+  const markup = "";
+  for (let i = 1; i <= countLesson; i++) {
+    markup = `<li>${i}. </li>  
+    <li>${lesson}</li>`;
+    cardSchedule.insertAdjacentHTML("beforeend", markup);
   }
-  console.log(date);
 }
