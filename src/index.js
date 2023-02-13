d0d524d93a7664c687030609ee39d98bd07b1dda
KhailoAndrey <hap512@gmail.com>
2023-02-13 18:15:58 +0200

    project

--- src/index.js
+++ src/index.js
@@ -7,13 +7,14 @@ const submitBtn = document.getElementById("submit-form");
 const dateForm = document.getElementById("datetime-picker");
 const cardSchedule = document.getElementById("schedule-list");
 const inputLesson = document.getElementById("input-lesson");
+const teacherBtn = document.getElementById("enter-btn");
 
 let userDate = new Date().toLocaleDateString();
 // let userDateArray =[];
 
 const options = {
   defaultDate: new Date(),
-  // dateFormat: "Y-d-m",
+  dateFormat: "d-m-Y",
   onClose(dateStr) {
     userDate = dateStr[0].toLocaleDateString();
   },
@@ -26,7 +27,6 @@ submitBtn.addEventListener("click", submitHandler);
 
 function submitHandler(e) {
   e.preventDefault();
-
   const schedule = {
     date: userDate,
     lesson: inputLesson.value,
@@ -41,19 +41,31 @@ function submitHandler(e) {
 }
 
 function createCardSchedule(array) {
-  console.log(array);
-  //   const markup = array
-  //     .map((arr) => {
-  //       `<li>${arr.lesson}</li>`
-  //     }).join("");
-  //     console.log(markup);
-  //   cardSchedule.insertAdjacentHTML("beforeend", markup);
-
   cardSchedule.innerHTML = "";
-  // let markup = "";
-  for (let i = 0; i <= array.length; i++) {
-    const markup = `<li>${array[i].lesson}</li>`;
-    cardSchedule.innerHTML += markup;
-    console.log(markup);
-  }
+  console.log(array);
+  const markup = array.map((arr) => `<li>${arr.lesson}</li>`).join("");
+  // console.log(markup);
+  cardSchedule.insertAdjacentHTML("beforeend", markup);
+
+  // cardSchedule.innerHTML = "";
+  // // let markup = "";
+  // for (let i = 0; i <= array.length; i++) {
+  //   const markup = `<li>${array[i].lesson}</li>`;
+  //   cardSchedule.innerHTML += markup;
+  //   console.log(markup);
+  // }
+}
+
+teacherBtn.addEventListener("click", activateModal);
+
+function activateModal() {
+  // initialize modal element
+  var modalEl = document.createElement("div");
+  modalEl.style.width = "400px";
+  modalEl.style.height = "300px";
+  modalEl.style.margin = "100px auto";
+  modalEl.style.backgroundColor = "#fff";
+
+  // show modal
+  mui.overlay("on", modalEl);
 }
